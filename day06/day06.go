719d271295fb69ae2e4c23121cf13b1374325d30
Matt Mohan <matt.mohan@wishabi.com>
2021-12-06 09:38:42 -0500

    Day6

--- day06/day06.go
+++ day06/day06.go
@@ -1,28 +1,61 @@
 package main
 
 import (
-    "fmt"
+	"fmt"
+	"strconv"
+	"strings"
 
-    "github.com/mattmohan/advent2021/pkg/advent_helpers"
+	"github.com/mattmohan/advent2021/pkg/advent_helpers"
 )
 
-type PuzzleInput []string
+type PuzzleInput []int
 
 func main() {
 	inputLines := advent_helpers.ReadInput("day06/input.txt")
-	
-	fmt.Println("day06 Part a: ", SolveA(inputLines))
-	fmt.Println("day06 Part b: ", SolveB(inputLines))
+
+	fmt.Println("day06 Part a: ", SolveA(ProcessInput(inputLines)))
+	fmt.Println("day06 Part b: ", SolveB(ProcessInput(inputLines)))
 }
 
 func ProcessInput(input []string) PuzzleInput {
-    return input
+	splitInput := strings.Split(input[0], ",")
+	ints := make([]int, 0, len(splitInput))
+	for _, i := range splitInput {
+		number, _ := strconv.Atoi(i)
+		ints = append(ints, number)
+	}
+
+	return ints
 }
 
-func SolveA(input PuzzleInput) string {
-	return "Not yet implemented"
+func SolveA(fish PuzzleInput) string {
+	return Solve(fish, 80)
 }
 
-func SolveB(input PuzzleInput) string {
-	return "Not yet implemented"
+func SolveB(fish PuzzleInput) string {
+	return Solve(fish, 256)
+}
+
+func Solve(fish PuzzleInput, days int) string {
+	sum := 0
+	tally := [9]int{}
+
+	for _, i := range fish {
+		tally[i]++
+	}
+
+	for i := 0; i < days; i++ {
+		newFish := tally[0]
+		for j := 1; j <= 8; j++ {
+			tally[j-1] = tally[j]
+		}
+		tally[8] = 0
+		tally[6] += newFish
+		tally[8] += newFish
+	}
+	for _, i := range tally {
+		sum += i
+	}
+
+	return fmt.Sprint(sum)
 }
--- day06/day06_test.go
+++ day06/day06_test.go
@@ -1,27 +1,27 @@
 package main
 
 import (
-    "strings"
-    "testing"
+	"strings"
+	"testing"
 )
 
 func TestA(t *testing.T) {
-    testInput := ``
-    expected := "N/A"
+	testInput := `3,4,3,1,2`
+	expected := "5934"
 
-    soln := SolveA(ProcessInput(strings.Split(testInput, "\n")))
+	soln := SolveA(ProcessInput(strings.Split(testInput, "\n")))
 
-    if soln != expected {
-        t.Errorf("day06 Part A doesn't match: %s != %s", soln, expected)
-    }
+	if soln != expected {
+		t.Errorf("day06 Part A doesn't match: %s != %s", soln, expected)
+	}
 }
 func TestB(t *testing.T) {
-    testInput := ``
-    expected := "N/A"
+	testInput := `3,4,3,1,2`
+	expected := "26984457539"
 
-    soln := SolveB(ProcessInput(strings.Split(testInput, "\n")))
+	soln := SolveB(ProcessInput(strings.Split(testInput, "\n")))
 
-    if soln != expected {
-        t.Errorf("day06 Part B doesn't match: %s != %s", soln, expected)
-    }
+	if soln != expected {
+		t.Errorf("day06 Part B doesn't match: %s != %s", soln, expected)
+	}
 }
